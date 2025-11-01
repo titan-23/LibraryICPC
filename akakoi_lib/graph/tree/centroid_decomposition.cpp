8f7b5b277bcfd8459a7624ee24387ec47a5603e0
titan-23 <titan.kaneda@gmail.com>
2025-11-01 12:18:36 +0900

    [LIB] 重心分解を追加

---
+++ akakoi_lib/graph/tree/centroid_decomposition.cpp
@@ -0,0 +1,60 @@
+struct CentroidDecomposition {
+private:
+    int C;
+    vector<vector<int>> G, T;
+    vector<int> sub;
+    vector<bool> banned;
+    int dfs(int v, int p) {
+        sub[v] = 1;
+        for (int x : G[v]) {
+            if (x == p || banned[x]) continue;
+            sub[v] += dfs(x, v);
+        }
+        return sub[v];
+    }
+    int find_centroid(int v, int p, int mid) {
+        for (int x : G[v]) {
+            if (x == p || banned[x]) continue;
+            if (sub[x] > mid) return find_centroid(x, v, mid);
+        }
+        return v;
+    }
+    int build(int v) {
+        int total = dfs(v, -1);
+        int c = find_centroid(v, -1, total/2);
+        banned[c] = true;
+        for (int x : G[c]) if (!banned[x]) {
+            int w = build(x);
+            T[c].push_back(w);
+            T[w].push_back(c);
+        }
+        return c;
+    }
+    template <typename F>
+    void inner_solve(int v, int p, F& func) {
+        func(v, banned);
+        banned[v] = true;
+        for (int x : T[v]) if (x != p) {
+            inner_solve(x, v, func);
+        }
+    }
+public:
+    CentroidDecomposition(vector<vector<int>> G) : G(G) {
+        int n = G.size();
+        T.resize(n);
+        sub.resize(n);
+        banned.resize(n, false);
+        banned.resize(n, false);
+        C = build(0);
+    }
+    pair<int, vector<vector<int>>> get_result() {return {C, T}; }
+    // vをまたぐパスについて考える vのみ / vを端点 / vを端点としない などの場合分け
+    // パスの重複に注意 / 元の木と重心分解の木の混同に注意
+    // bannedを見てはいけない
+    // cd.solve([&] (int v, const vector<bool>& banned) {});
+    template <typename F>
+    void solve(F func) {
+        fill(banned.begin(), banned.end(), false);
+        inner_solve(C, -1, func);
+    }
+};
