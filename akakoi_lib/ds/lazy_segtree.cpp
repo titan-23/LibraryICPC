597bc89a55bdef57a979485ae742c9505e93fca1
titan-23 <titan.kaneda@gmail.com>
2025-11-08 03:05:30 +0900

    バグ修正

--- akakoi_lib/ds/lazy_segtree.cpp
+++ akakoi_lib/ds/lazy_segtree.cpp
@@ -4,21 +4,23 @@ private:
   int n, log, s;
   vector<T> d;
   vector<F> lazy;
-  void update(int k) {  d[k] = op(d[k<<1], d[k<<1|1]); }
+  void update(int k) { d[k] = op(d[k<<1], d[k<<1|1]); }
   void all_apply(int k, F f) {
     d[k] = mapping(f, d[k]);
     if (k < s) lazy[k] = composition(f, lazy[k]);
   }
+  // for beats
+  // void all_apply(int k, F f) {
+  //     d[k] = mapping(f, d[k]);
+  //     if (k < size) {
+  //         lz[k] = composition(f, lz[k]);
+  //         if (d[k].fail) push(k), update(k);
+  //     }
+  // }
   void propagate(int k) {
     all_apply(k<<1, lazy[k]);
     all_apply(k<<1|1, lazy[k]);
     lazy[k] = id();
-  }
-  void upper_push(int l, int r) {
-    for (int i = log; i > 0; --i) {
-      if ((l>>i<<i) != l) propagate(l>>i);
-      if ((r>>i<<i) != r) propagate((r-1)>>i);
-    }
   }
 public:
   LazySegtree(int n) : n(n) {
@@ -46,7 +48,10 @@ public:
   void apply(int l, int r, F f) {
     if (l == r) return;
     l += s; r += s;
-    upper_push(l, r);
+    for (int i = log; i > 0; --i) {
+      if ((l>>i<<i) != l) propagate(l>>i);
+      if ((r>>i<<i) != r) propagate((r-1)>>i);
+    }
     int pl = l, pr = r;
     while (l < r) {
       if (l & 1) all_apply(l++, f);
@@ -65,7 +70,10 @@ public:
   T prod(int l, int r) {
     if (l == r) return e();
     l += s; r += s;
-    upper_push(l, r);
+    for (int i = log; i > 0; --i) {
+      if ((l>>i<<i) != l) propagate(l>>i);
+      if ((r>>i<<i) != r) propagate(r>>i);
+    }
     T lv = e(), rv = e();
     while (l < r) {
       if (l & 1) lv = op(lv, d[l++]);
@@ -87,10 +95,10 @@ public:
     d[k] = v;
     for (int i = 1; i <= log; ++i) update(k>>i);
   }
-  template<typename F>
-  int max_right(int l, F f) {
+  template<typename G>
+  int max_right(int l, G f) {
     assert(f(e()));
-    if (l == s) return n;
+    if (l == n) return n;
     l += s;
     for (int i = log; i > 0; --i) propagate(l>>i);
     T v = e();
@@ -112,12 +120,12 @@ public:
     } while ((l & -l) != l);
     return n;
   }
-  template<typename F>
-  int min_left(int r, F f) {
+  template<typename G>
+  int min_left(int r, G f) {
     assert(f(e()));
     if (r == 0) return 0;
     r += s;
-    for (int i = log; i > 0; --i) propagate((r - 1) >> i);
+    for (int i = log; i > 0; --i) propagate((r-1)>>i);
     T v = e();
     do {
       r--;
