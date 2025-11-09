d7cf447633a35aa67abaabeacf0340c7ec461998
titan-23 <titan.kaneda@gmail.com>
2025-11-09 18:59:16 +0900

    テスト追加

--- akakoi_lib/graph/max_independent_set.cpp
+++ akakoi_lib/graph/max_independent_set.cpp
@@ -1,47 +1,36 @@
-unsigned long long MaxIndependentSet(vector<vector<int>> g) {
-  using U = unsigned long long;
-  int n = g.size();
-  vector<U> nbd(n);
-  for(int i = 0; i < n; ++i) {
-    for(auto e : g[i]) { nbd[i] |= 1ULL << e; }
+using ull = unsigned long long;
+ull maximum_independent_set(vector<vector<int>> G) {
+  const int n = G.size();
+  vector<ull> E(n);
+  rep(i, n) {
+    for (auto& e : G[i]) E[i] |= 1ull << e;
   }
-  int best = 0;
-  U res = 0;
-  auto dfs = [&](auto&& dfs, U now, U rest) -> void {
-    pair<int, int> p(-1, -1);
-    while(true) {
-      bool upd = false;
-      for(int v = 0; v < n; ++v) {
-        if(rest >> v & 1) {
-          int d = __builtin_popcount(nbd[v] & rest);
-          if(d > p.second) {
-            p.second = d;
-            p.first = v;
-          }
-          if(d <= 1) {
-            rest ^= 1ULL << v;
-            rest &= ~nbd[v];
-            now |= 1ULL << v;
-            upd = true;
-          }
+  int ans = 0;
+  ull res = 0;
+  auto dfs = [&] (auto&& dfs, ull now, ull rest) -> void {
+    pair<int, int> p = {-1, -1};
+    while (true) {
+      bool f = false;
+      rep(v, n) if ((rest >> v) & 1) {
+        int deg = popcount(E[v] & rest);
+        chmax(p, pair<int, int>{deg, v});
+        if (deg <= 1) {
+          rest ^= 1ull << v, rest &= ~E[v], now |= 1ull << v;
+          f = true;
         }
       }
-      if(!upd) break;
+      if (!f) break;
       p = {-1, -1};
     }
-    int a = __builtin_popcount(now), b = __builtin_popcount(rest);
-    if(a > best) {
-      best = a;
-      res = now;
-    }
-    if(b == 0 || a + b <= best) return;
-    int v = p.first;
-    rest &= ~(1ULL << v);
-    if(p.second >= 3) dfs(dfs, now, rest);
-    now |= 1ULL << v;
-    dfs(dfs, now, rest & ~(nbd[v]));
+    const int sz1 = popcount(now), sz2 = popcount(rest);
+    if (chmax(ans, sz1)) res = now;
+    if (sz2 == 0 || sz1 + sz2 <= ans) return;
+    const int v = p.second;
+    rest &= ~(1ull << v);
+    dfs(dfs, now, rest);
+    now |= 1ull << v;
+    dfs(dfs, now, rest & (~E[v]));
   };
-  U now = 0, rest = (1ULL << n) - 1;
-  dfs(dfs, now, rest);
+  dfs(dfs, 0, (1ull << n) - 1);
   return res;
 }
---
+++ test/graph/mxind.cpp
@@ -0,0 +1,25 @@
+// https://judge.yosupo.jp/submission/327570
+#include "./../../akakoi_lib/template/template.cpp"
+#include "./../../akakoi_lib/graph/max_independent_set.cpp"
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<vector<int>> G(n);
+    rep(i, m) {
+        int u, v; cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    auto X = maximum_independent_set(G);
+    cout << __builtin_popcountll(X) << "\n";
+    rep(i, n) {
+        if (X >> i & 1) cout << i << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}
