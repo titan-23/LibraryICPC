90df2947c58701b1df0781e293288c79233571d2
titan-23 <titan.kaneda@gmail.com>
2025-11-09 19:18:10 +0900

    twosatテスト追加

---
+++ test/graph/twosat.cpp
@@ -0,0 +1,32 @@
+// https://judge.yosupo.jp/submission/327576
+#include "./../../akakoi_lib/template/template.cpp"
+#include "./../../akakoi_lib/graph/scc.cpp"
+#include "./../../akakoi_lib/graph/twosat.cpp"
+
+void solve() {
+    string x; cin >> x >> x;
+    int n, m; cin >> n >> m;
+    TwoSat sat(n+1);
+    rep(i, m) {
+        int a, b; cin >> a >> b;
+        int x; cin >> x;
+        sat.add(abs(a), a>0, abs(b), b>0);
+    }
+    vector<bool> res = sat.solve();
+    if (res.empty()) {
+        cout << "s UNSATISFIABLE" << "\n";
+        return;
+    }
+    cout << "s SATISFIABLE" << "\n";
+    cout << "v ";
+    for (int i = 1; i <= n; ++i) {
+        cout << (res[i] > 0 ? i : -i) << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+}
