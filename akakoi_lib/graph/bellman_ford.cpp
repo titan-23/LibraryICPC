b04488ca1226a9a06ee48f7b9853bbd77e10c2f6
titan-23 <titan.kaneda@gmail.com>
2025-11-11 11:05:42 +0900

    ベルマンフォード追加

---
+++ akakoi_lib/graph/bellman_ford.cpp
@@ -0,0 +1,14 @@
+vector<ll> bellman_ford(vector<vector<pair<int, ll>>> G, int s) {
+  int n = G.size();
+  vector<ll> dist(n, INF); dist[s] = 0;
+  bool negcycle = n > 0;
+  rep(i, n) {
+    bool upd = false;
+    rep(v, n) for (auto [x, c] : G[v]) {
+      if (chmin(dist[x], dist[v]+c)) upd = true;
+    }
+    if (!upd) { negcycle = false; break; }
+  }
+  if (negcycle) return {};
+  return dist;
+}
