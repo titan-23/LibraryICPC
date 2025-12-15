464a30e1be80aa9f76713388e429dd244b1147b9
titan-23 <titan.kaneda@gmail.com>
2025-12-15 11:58:41 +0900

    履歴保持用のカスofコミット

--- akakoilib/gomi/p-wbtree.cpp
+++ akakoilib/gomi/p-wbtree.cpp
@@ -39,39 +39,39 @@ class PLWT {
 private:
   static constexpr int DELTA = 3, GAMMA = 2;
   U root;
-  inline U weight_right(U node) const { return ch[ch[node].right].size + 1; }
-  inline U weight_left(U node) const { return ch[ch[node].left].size + 1; }
-  inline U weight(U node) const { return ch[node].size + 1; }
-  void update(U node) {
-    ch[node].size = 1 + ch[ch[node].left].size + ch[ch[node].right].size;
-    dat[node].data = dat[node].key;
-    if (ch[node].left) dat[node].data = op(dat[ch[node].left].data, dat[node].key);
-    if (ch[node].right) dat[node].data = op(dat[node].data, dat[ch[node].right].data);
+  inline U weight_right(U node) { return ch[ch[node].right].size + 1; }
+  inline U weight_left(U node) { return ch[ch[node].left].size + 1; }
+  inline U weight(U node) { return ch[node].size + 1; }
+  void update(U v) {
+    ch[v].size = 1 + ch[ch[v].left].size + ch[ch[v].right].size;
+    dat[v].data = dat[v].key;
+    if (ch[v].left) dat[v].data = op(dat[ch[v].left].data, dat[v].key);
+    if (ch[v].right) dat[v].data = op(dat[v].data, dat[ch[v].right].data);
   }
-  void push(U node, F f) {
-    dat[node].key = mapping(f, dat[node].key);
-    dat[node].data = mapping(f, dat[node].data);
-    dat[node].lazy = composition(f, dat[node].lazy);
+  void push(U v, F f) {
+    dat[v].key = mapping(f, dat[v].key);
+    dat[v].data = mapping(f, dat[v].data);
+    dat[v].lazy = composition(f, dat[v].lazy);
   }
-  void propagate(U node) {
-    if (dat[node].rev) {
-      U l = ch[node].left ? node_copy(ch[node].left) : 0;
-      U r = ch[node].right ? node_copy(ch[node].right) : 0;
-      ch[node].left = r; ch[node].right = l;
-      if (ch[node].left) dat[ch[node].left].rev ^= 1;
-      if (ch[node].right) dat[ch[node].right].rev ^= 1;
-      dat[node].rev = 0;
+  void propagate(U v) {
+    if (dat[v].rev) {
+      U l = ch[v].left ? node_copy(ch[v].left) : 0;
+      U r = ch[v].right ? node_copy(ch[v].right) : 0;
+      ch[v].left = r; ch[v].right = l;
+      if (ch[v].left) dat[ch[v].left].rev ^= 1;
+      if (ch[v].right) dat[ch[v].right].rev ^= 1;
+      dat[v].rev = 0;
     }
-    if (dat[node].lazy != id()) {
-      if (ch[node].left) {
-        ch[node].left = node_copy(ch[node].left);
-        push(ch[node].left, dat[node].lazy);
+    if (dat[v].lazy != id()) {
+      if (ch[v].left) {
+        ch[v].left = node_copy(ch[v].left);
+        push(ch[v].left, dat[v].lazy);
       }
-      if (ch[node].right) {
-        ch[node].right = node_copy(ch[node].right);
-        push(ch[node].right, dat[node].lazy);
+      if (ch[v].right) {
+        ch[v].right = node_copy(ch[v].right);
+        push(ch[v].right, dat[v].lazy);
       }
-      dat[node].lazy = id();
+      dat[v].lazy = id();
     }
   }
   // assert(weight_left(node)*DELTA>=weight_right(node));
@@ -79,55 +79,51 @@ private:
   void _build(vector<T> const &a) {
     auto dfs = [&] (auto &&dfs, U l, U r) -> U {
       U mid = (l + r) >> 1;
-      U node = new_node(a[mid], id());
-      if (l != mid) ch[node].left = dfs(dfs, l, mid);
-      if (mid+1 != r) ch[node].right = dfs(dfs, mid+1, r);
-      update(node); return node;
+      U v = new_node(a[mid], id());
+      if (l != mid) ch[v].left = dfs(dfs, l, mid);
+      if (mid+1 != r) ch[v].right = dfs(dfs, mid+1, r);
+      update(v); return v;
     };
     if (a.empty()) { root = 0; return; }
     root = dfs(dfs, 0, (U)a.size());
   }
-  U _rotate_right(U node) {
-    U u = node_copy(ch[node].left);
-    ch[node].left = ch[u].right; ch[u].right = node;
-    update(node); update(u); return u;
+  U _rotate_right(U x) {
+    U u = x_copy(ch[x].left);
+    ch[x].left = ch[u].right; ch[u].right = x;
+    update(x); update(u); return u;
   }
-  U _rotate_left(U node) {
-    U u = node_copy(ch[node].right);
-    ch[node].right = ch[u].left; ch[u].left = node;
-    update(node); update(u); return u;
+  U _rotate_left(U x) {
+    U u = x_copy(ch[x].right);
+    ch[x].right = ch[u].left; ch[u].left = x;
+    update(x); update(u); return u;
   }
-  U _balance_left(U node) {
-    propagate(ch[node].right);
-    U u = ch[node].right;
-    if (weight_left(ch[node].right) >= weight_right(ch[node].right) * GAMMA) {
+  U _balance_left(U x) {
+    propagate(ch[x].right); U u = ch[x].right;
+    if (weight_left(ch[x].right) >= weight_right(ch[x].right) * GAMMA) {
       propagate(ch[u].left);
-      ch[node].right = _rotate_right(u);
+      ch[x].right = _rotate_right(u);
     }
-    u = _rotate_left(node); return u;
+    u = _rotate_left(x); return u;
   }
-  U _balance_right(U node) {
-    propagate(ch[node].left);
-    U u = ch[node].left;
-    if (weight_right(ch[node].left) >= weight_left(ch[node].left) * GAMMA) {
+  U _balance_right(U x) {
+    propagate(ch[x].left); U u = ch[x].left;
+    if (weight_right(ch[x].left) >= weight_left(ch[x].left) * GAMMA) {
       propagate(ch[u].right);
-      ch[node].left = _rotate_left(u);
+      ch[x].left = _rotate_left(u);
     }
-    u = _rotate_right(node); return u;
+    u = _rotate_right(x); return u;
   }
   U _merge_with_root(U l, U root, U r) {
     if (weight(r) * DELTA < weight(l)) {
       propagate(l); l = node_copy(l);
       ch[l].right = _merge_with_root(ch[l].right, root, r);
       update(l);
-      if (weight(ch[l].left) * DELTA < weight(ch[l].right)) return _balance_left(l);
-      return l;
+      return weight(ch[l].left)*DELTA < weight(ch[l].right) ? _balance_left(l) : l;
     } else if (weight(l) * DELTA < weight(r)) {
       propagate(r); r = node_copy(r);
       ch[r].left = _merge_with_root(l, root, ch[r].left);
       update(r);
-      if (weight(ch[r].right) * DELTA < weight(ch[r].left)) return _balance_right(r);
-      return r;
+      return weight(ch[r].right)*DELTA < weight(ch[r].left) ? _balance_right(r) : r;
     }
     root = node_copy(root);
     ch[root].left = l; ch[root].right = r;
@@ -142,23 +138,23 @@ private:
     auto [l_, root_] = _pop_right(l);
     return _merge_with_root(l_, root_, r);
   }
-  pair<U, U> _split_node(U node, U k) {
-    if (!node) return {0, 0};
-    propagate(node);
-    U lch = ch[node].left, rch = ch[node].right;
+  pair<U, U> _split_node(U x, U k) {
+    if (!x) return {0, 0};
+    propagate(x);
+    U lch = ch[x].left, rch = ch[x].right;
     U tmp = lch ? k-ch[lch].size : k;
     if (tmp == 0) {
-      return {lch, _merge_with_root(0, node, rch)};
+      return {lch, _merge_with_root(0, x, rch)};
     } else if (tmp < 0) {
       auto [l, r] = _split_node(lch, k);
-      return {l, _merge_with_root(r, node, rch)};
+      return {l, _merge_with_root(r, x, rch)};
     } else {
       auto [l, r] = _split_node(rch, tmp-1);
-      return {_merge_with_root(lch, node, l), r};
+      return {_merge_with_root(lch, x, l), r};
     }
   }
   PLWT(U root) : root(root) {}
- public:
+public:
   PLWT() : root(0) {}
   PLWT(vector<T> &a) { _build(a); }
   PLWT merge(PLWT other) {
@@ -171,26 +167,26 @@ private:
   }
   PLWT apply(U l, U r, F f) {
     if (l == r) return PLWT(node_copy(root));
-    auto dfs = [&] (auto &&dfs, U node, U left, U right) -> U {
-      if (right <= l || r <= left) return node;
-      propagate(node); U nnode = node_copy(node);
-      if (l <= left && right <= r) { push(nnode, f); return nnode; }
-      U lsize = ch[ch[nnode].left].size;
-      if (ch[nnode].left) ch[nnode].left = dfs(dfs, ch[nnode].left, left, left+lsize);
-      if (l <= left+lsize && left+lsize < r) dat[nnode].key = mapping(f, dat[nnode].key);
-      if (ch[nnode].right) ch[nnode].right = dfs(dfs, ch[nnode].right, left+lsize+1, right);
-      update(nnode); return nnode;
+    auto dfs = [&] (auto &&dfs, U x, U left, U right) -> U {
+      if (right <= l || r <= left) return x;
+      propagate(x); U nx = node_copy(x);
+      if (l <= left && right <= r) { push(nx, f); return nx; }
+      U lsize = ch[ch[nx].left].size;
+      if (ch[nx].left) ch[nx].left = dfs(dfs, ch[nx].left, left, left+lsize);
+      if (l <= left+lsize && left+lsize < r) dat[nx].key = mapping(f, dat[nx].key);
+      if (ch[nx].right) ch[nx].right = dfs(dfs, ch[nx].right, left+lsize+1, right);
+      update(nx); return nx;
     }; return PLWT(dfs(dfs, root, 0, len()));
   }
   T prod(U l, U r) { if (l == r) return e();
-    auto dfs = [&] (auto &&dfs, U node, U left, U right) -> T {
+    auto dfs = [&] (auto &&dfs, U x, U left, U right) -> T {
       if (right <= l || r <= left) return e();
-      if (l <= left && right <= r) return dat[node].data;
-      propagate(node);
-      U lsize = ch[ch[node].left].size; T res = e();
-      if (ch[node].left) res = dfs(dfs, ch[node].left, left, left+lsize);
-      if (l <= left+lsize && left+lsize < r) res = op(res, dat[node].key);
-      if (ch[node].right) res = op(res, dfs(dfs, ch[node].right, left+lsize+1, right));
+      if (l <= left && right <= r) return dat[x].data;
+      propagate(x);
+      U lsize = ch[ch[x].left].size; T res = e();
+      if (ch[x].left) res = dfs(dfs, ch[x].left, left, left+lsize);
+      if (l <= left+lsize && left+lsize < r) res = op(res, dat[x].key);
+      if (ch[x].right) res = op(res, dfs(dfs, ch[x].right, left+lsize+1, right));
       return res;
     }; return dfs(dfs, root, 0, len());
   }
@@ -246,12 +242,12 @@ private:
     update(nxt); return nxt;
   }
   T get(U k) {
-    U node = root;
+    U v = root;
     while (1) {
-      propagate(node); U t = ch[ch[node].left].size;
-      if (t == k) { return dat[node].key; }
-      if (t < k) { k -= t + 1; node = ch[node].right; }
-      else { node = ch[node].left; }
+      propagate(v); U t = ch[ch[v].left].size;
+      if (t == k) return dat[v].key;
+      if (t < k) { k -= t + 1; v = ch[v].right; }
+      else { v = ch[v].left; }
     }
   }
   U len() const { return ch[root].size; }
